c12fc870aa6fb6a29751d30436754ffebf2e4e9e
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-19 13:32:44 -0800

    figlet and input text appears in cool figlet font!

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,49 @@
 /* eslint-disable no-console */
 import React, { PureComponent } from 'react';
+import figlet from 'figlet';
 
 export default class App extends PureComponent {
 
   state = {
-    // hi: 'there'
     counter: 0,
     text: '',
-    anotherText: ''
+    anotherText: '',
+    formattedText: ''
   }
 
   handleClick = () => {
-    // console.log(this.state.hi);
-
-    // to start counter at 0:
-    // this.setState((state) => {
-    //   console.log('Clicked ' + this.state.counter + ' times');
-    //   return { counter: state.counter + 1 };
-    // });
-    // or, to start at 1 instead of 0:
     const { counter } = this.state;
     this.setState({ counter: counter + 1 }, () => {
       console.log('Clicked ' + this.state.counter + ' times');
     });
-
   }
+
+  formatText = () => {
+    const font = 'Weird';
+    figlet.text(this.state.text, { font }, (error, data) => {
+      if(error) return console.log(error);
+      this.setState({ formattedText: data });
+    });
+  }
+
   handleChange = ({ target }) => {
-    console.log(target.name);
-    this.setState({ [target.name]: target.value });
+    // console.log(target.name);
+    this.setState({ [target.name]: target.value }, () => {
+      this.formatText();
+    });
   }
 
   render() {
-    const { text, anotherText } = this.state;
+    const { text, anotherText, formattedText } = this.state;
     return (
       < >
         <h1>Hi</h1>
         <input type="text" name="text" value={text} onChange={this.handleChange} />
         <input type="text" name="anotherText" value={anotherText} onChange={this.handleChange} />
+        <input type="text" name="text" value={text} onChange={this.handleChange} />
         <h1>{text}</h1>
         <h1>{anotherText}</h1>
+        <h1><pre>{formattedText}</pre></h1>
         <button onClick={this.handleClick}>Submit</button>
       </>
     );
