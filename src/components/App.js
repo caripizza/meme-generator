38aa05fa56296db60bc695d1e49a1820a9a8f5b3
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-19 11:34:05 -0800

    added counter for App prints to console

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
+/* eslint-disable no-console */
 import React, { PureComponent } from 'react';
 
 export default class App extends PureComponent {
+
+  state = {
+    // hi: 'there'
+    counter: 0
+  }
+
+  handleClick = () => {
+    // console.log(this.state.hi);
+    // to start counter at 0:
+    // this.setState((state) => {
+    //   console.log('Clicked ' + this.state.counter + ' times');
+    //   return { counter: state.counter + 1 };
+    // });
+    // or, to start at 1 instead of 0:
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 }, () => {
+      console.log('Clicked ' + this.state.counter + ' times');
+    });
+
+  }
+
   render() {
     return (
       < >
         <h1>Hi</h1>
-        <button>Submit</button>
+        <button onClick={this.handleClick}>Submit</button>
       </>
     );
   }
 }
+
 
 // function App() {
 // return (
