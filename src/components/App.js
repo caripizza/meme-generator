f406381b0a99dbb3bff30c3e3444d52c6b18cfdd
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-19 10:48:29 -0800

    added scaffolding and assets

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      < >
+        <h1>Hi</h1>
+        <button>Submit</button>
+      </>
+    );
+  }
+}
+
+// function App() {
+// return (
+// <h1>Hi</h1>
+// );
+// }
