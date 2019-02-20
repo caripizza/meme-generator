22de0a6de04c0555c626852ae51433ef56110c65
Cari Palazzolo <cari.pizza@gmail.com>
2019-02-19 16:16:59 -0800

    add logic to App, header/image/footer text appears on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
 /* eslint-disable no-console */
 import React, { PureComponent } from 'react';
 import AppDemo from './AppDemo';
+// import 'normalize.css';
 
 export default class App extends PureComponent {
   state = {
-    header: '',
-    footer: '',
+    headerText: '',
+    footerText: '',
     imageUrl: '',
     meme: ''
   }
 
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value }, () => {
+      this.headerText, this.footerText;
+    });
+  }
+
   render() {
+    const { headerText, footerText, imageUrl } = this.state;
     return (
       < >
         <AppDemo />
+
         <h1>App component</h1>
+        <div>
+          <input type="text" name="headerText" value={headerText} onChange={this.handleChange}/>
+          <input type="text" name="imageUrl" value={imageUrl} onChange={this.handleChange}/>
+          <input type="text" name="footerText" value={footerText} onChange={this.handleChange}/>
+          <h3>{headerText}</h3>
+          <img src={imageUrl}/>
+          <h3>{footerText}</h3>
+        </div>
       </>
     );
   }
